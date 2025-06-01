4851a009d8a670d7e09af8727bc9fd7fd27d3675
AbdelRahman <abdelrahman.rahal.mail@gmail.com>
2025-06-01 05:08:31 +0300

    fix: removed redundant comment

--- stockdb/client/src/App.jsx
+++ stockdb/client/src/App.jsx
@@ -13,27 +13,3 @@ function App() {
 }
 
 export default App
-
-// Example of how to fetch data from the server
-// function App() {
-//   const [items, setItems] = useState([]);
-
-//   useEffect(() => {
-//     fetch('http://localhost:5000/api/items')
-//       .then(res => res.json())
-//       .then(data => setItems(data));
-//   }, []);
-
-//   return (
-//     <div className="p-4">
-//       <h1 className="text-2xl font-bold">Inventory</h1>
-//       <ul>
-//         {items.map(item => (
-//           <li key={item.id}>{item.name} - {item.quantity} in stock</li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
-
-// export default App;
